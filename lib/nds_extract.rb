ca6e9484f961ed87b80f42a40f9b73ff53170a23
William Kennedy <alexkenned@gmail.com>
2020-02-26 18:59:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,15 +13,15 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   list = {}
-  directors_index = 0 
+directors_index = 0 
   while directors_index < nds.length do 
     title_index = 0 
     grand_total = 0 
-    while title_index < director_data[directors_index][:movies].length do
-      grand_total += director_data[directors_index][:movies][title_index][:worldwide_gross]
+    while title_index < nds[directors_index][:movies].length do
+      grand_total += nds[directors_index][:movies][title_index][:worldwide_gross]
       title_index+=1
     end
-    list["#{director_data[directors_index][:name]}"] = grand_total
+    list["#{nds[directors_index][:name]}"] = grand_total
     directors_index +=1 
  end
  pp list
